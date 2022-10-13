86bb827cca9112cfef8b1452da4f93e019a07a57
Olivier Basille <obasille@hotmail.com>
2022-10-13 18:24:31 +0200

    chore: configure linting for js config files, remove unwanted eslint config files from all packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,27 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   overrides: [
+    // Typescript
     {
       files: ["*.ts", "*.tsx", "*.d.ts"],
       parserOptions: {
         project: "./tsconfig.eslint.json",
       },
     },
+    // Configuration files
+    {
+      files: [
+        "*.config.js",
+        "**/config/**/*.js",
+        "**/scripts/**/*.js",
+        "**/tools/**/*.js",
+      ],
+      env: {
+        node: true,
+      },
+    },
   ],
+  // React Native Vision Camera
   rules: {
     "react-hooks/exhaustive-deps": [
       "warn",
