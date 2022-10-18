bc827344f168950243fddd3b443c27a7b2f829e5
Olivier Basille <obasille@hotmail.com>
2022-10-18 17:21:36 +0200

    chore(rn-nordic-nrf5-dfu): copy source code from standalone repo

---
+++ apps/test-react-native-nordic-nrf5-dfu/android/app/src/main/java/com/systemicgames/nordicnrf5dfuexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.systemicgames.nordicnrf5dfuexample;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "NordicNrf5DfuExample";
+  }
+}
---
+++ apps/test-react-native-nordic-nrf5-dfu/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  NordicNrf5DfuExample
+//
+
+import Foundation
