a42f04ac14fbc696c3ede692c0df7cb43b261778
Olivier Basille <obasille@hotmail.com>
2022-10-18 14:44:40 +0200

    chore: update test-react-native-bluetooth-le to build and run properly

--- apps/test-react-native-bluetooth-le/metro.config.js
+++ apps/test-react-native-bluetooth-le/metro.config.js
@@ -1,37 +1,10 @@
-const path = require("path");
-// https://stackoverflow.com/a/68245906
-//const blacklist = require("metro-config/src/defaults/blacklist");
-const blacklist = require("metro-config/src/defaults/exclusionList");
-const escape = require("escape-string-regexp");
-const pak = require("../package.json");
 const findWorkspaceRoot = require("find-yarn-workspace-root");
 
 const root = findWorkspaceRoot(__dirname);
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
-
 module.exports = {
   projectRoot: __dirname,
   watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`)
-      )
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, "node_modules", name);
-      return acc;
-    }, {}),
-  },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
