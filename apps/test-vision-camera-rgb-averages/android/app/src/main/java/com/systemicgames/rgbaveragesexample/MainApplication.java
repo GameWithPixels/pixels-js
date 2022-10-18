7abd7c92760081d5f003c118769fd10c98c1d54f
Olivier Basille <obasille@hotmail.com>
2022-10-18 21:33:31 +0200

    chore: udpate test-vision-camera-rgb-averages app config to work as standalone app

--- apps/test-vision-camera-rgb-averages/android/app/src/main/java/com/systemicgames/rgbaveragesexample/MainApplication.java
+++ apps/test-vision-camera-rgb-averages/android/app/src/main/java/com/systemicgames/rgbaveragesexample/MainApplication.java
@@ -30,9 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
                 protected List<ReactPackage> getPackages() {
                     @SuppressWarnings("UnnecessaryLocalVariable")
                     List<ReactPackage> packages = new PackageList(this).getPackages();
-                    // Packages that cannot be autolinked yet can be added manually here, for VisionCameraRgbAveragesExample:
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
                     // packages.add(new MyReactNativePackage());
-                    packages.add(new VisionCameraRgbAveragesPackage());
                     return packages;
                 }
 
