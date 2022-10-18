0b0bdb2e5622296e13908f265680197eceddee20
Olivier Basille <obasille@hotmail.com>
2022-10-18 12:22:10 +0200

    chore: update react native ble example metro config to work with workspace

--- packages/react-native-bluetooth-le/example/metro.config.js
+++ packages/react-native-bluetooth-le/example/metro.config.js
@@ -5,7 +5,7 @@ const blacklist = require("metro-config/src/defaults/exclusionList");
 const escape = require("escape-string-regexp");
 const pak = require("../package.json");
 
-const root = path.resolve(__dirname, "..");
+const root = path.resolve(__dirname, "../../..");
 
 const modules = Object.keys({
   ...pak.peerDependencies,
