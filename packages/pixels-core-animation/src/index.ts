99e29d75af874c96ccce3758737aa1a7e92f3ae4
Olivier Basille <obasille@hotmail.com>
2022-12-05 11:37:13 +0100

    refactor(core-animation): clean up index.ts

--- packages/pixels-core-animation/src/index.ts
+++ packages/pixels-core-animation/src/index.ts
@@ -1,107 +1,58 @@
-import DataSet from "./DataSet";
-import AnimationBits from "./animations/AnimationBits";
-import AnimationGradient from "./animations/AnimationGradient";
-import AnimationGradientPattern from "./animations/AnimationGradientPattern";
-import AnimationKeyframed from "./animations/AnimationKeyframed";
-import AnimationNoise from "./animations/AnimationNoise";
-import AnimationPreset from "./animations/AnimationPreset";
-import AnimationRainbow from "./animations/AnimationRainbow";
-import AnimationSimple from "./animations/AnimationSimple";
-import {
-  AnimationTypeValues,
-  type AnimationType,
-} from "./animations/AnimationType";
-import Constants from "./animations/Constants";
-import RgbKeyframe from "./animations/RgbKeyframe";
-import RgbTrack from "./animations/RgbTrack";
-import SimpleKeyframe from "./animations/SimpleKeyframe";
-import Track from "./animations/Track";
-import Color from "./color/Color";
-import Action from "./profiles/Action";
-import ActionPlayAnimation from "./profiles/ActionPlayAnimation";
-import ActionPlayAudioClip from "./profiles/ActionPlayAudioClip";
-import { ActionTypeValues, type ActionType } from "./profiles/ActionType";
-import Condition from "./profiles/Condition";
-import ConditionBatteryState, {
+// Animations
+export * from "./animations/AnimationType";
+export { default as AnimationBits } from "./animations/AnimationBits";
+export { default as AnimationGradient } from "./animations/AnimationGradient";
+export { default as AnimationGradientPattern } from "./animations/AnimationGradientPattern";
+export { default as AnimationKeyframed } from "./animations/AnimationKeyframed";
+export { default as AnimationNoise } from "./animations/AnimationNoise";
+export { default as AnimationPreset } from "./animations/AnimationPreset";
+export { default as AnimationRainbow } from "./animations/AnimationRainbow";
+export { default as AnimationSimple } from "./animations/AnimationSimple";
+export { default as RgbKeyframe } from "./animations/RgbKeyframe";
+export { default as RgbTrack } from "./animations/RgbTrack";
+export { default as SimpleKeyframe } from "./animations/SimpleKeyframe";
+export { default as Track } from "./animations/Track";
+
+// Profiles
+export { default as Profile } from "./profiles/Profile";
+export { default as Rule } from "./profiles/Rule";
+export { default as Action } from "./profiles/Action";
+export { default as ActionPlayAnimation } from "./profiles/ActionPlayAnimation";
+export { default as ActionPlayAudioClip } from "./profiles/ActionPlayAudioClip";
+export * from "./profiles/ConditionType";
+export {
+  default as ConditionBatteryState,
   BatteryStateFlagsValues,
   type BatteryStateFlags,
 } from "./profiles/ConditionBatteryState";
-import ConditionConnectionState, {
+export {
+  default as ConditionConnectionState,
   ConnectionStateFlagsValues,
   type ConnectionStateFlags,
 } from "./profiles/ConditionConnectionState";
-import ConditionCrooked from "./profiles/ConditionCrooked";
-import ConditionFaceCompare, {
+export {
+  default as ConditionFaceCompare,
   FaceCompareFlagsValues,
   type FaceCompareFlags,
 } from "./profiles/ConditionFaceCompare";
-import ConditionHandling from "./profiles/ConditionHandling";
-import ConditionHelloGoodbye, {
+export {
+  default as ConditionHelloGoodbye,
   HelloGoodbyeFlagsValues,
   type HelloGoodbyeFlags,
 } from "./profiles/ConditionHelloGoodbye";
-import ConditionIdle from "./profiles/ConditionIdle";
-import ConditionRolling from "./profiles/ConditionRolling";
-import {
-  ConditionTypeValues,
-  type ConditionType,
-} from "./profiles/ConditionType";
-import Profile from "./profiles/Profile";
-import Rule from "./profiles/Rule";
+export * from "./profiles/ActionType";
+export { default as Condition } from "./profiles/Condition";
+export { default as ConditionCrooked } from "./profiles/ConditionCrooked";
+export { default as ConditionHandling } from "./profiles/ConditionHandling";
+export { default as ConditionIdle } from "./profiles/ConditionIdle";
+export { default as ConditionRolling } from "./profiles/ConditionRolling";
 
-export {
-  AnimationBits,
-  AnimationGradient,
-  AnimationGradientPattern,
-  AnimationKeyframed,
-  AnimationNoise,
-  AnimationPreset,
-  AnimationRainbow,
-  AnimationSimple,
-  AnimationTypeValues,
-  type AnimationType,
-  Constants,
-  DataSet,
-  RgbKeyframe,
-  RgbTrack,
-  SimpleKeyframe,
-  Track,
-};
-
-export { Color };
-
-export {
-  ActionTypeValues,
-  type ActionType,
-  Action,
-  ActionPlayAnimation,
-  ActionPlayAudioClip,
-};
-
-export {
-  Condition,
-  ConditionTypeValues,
-  type ConditionType,
-  ConditionIdle,
-  ConditionRolling,
-  ConditionCrooked,
-  ConditionFaceCompare,
-  FaceCompareFlagsValues,
-  type FaceCompareFlags,
-  ConditionHandling,
-  ConditionHelloGoodbye,
-  HelloGoodbyeFlagsValues,
-  type HelloGoodbyeFlags,
-  ConditionConnectionState,
-  ConnectionStateFlagsValues,
-  type ConnectionStateFlags,
-  ConditionBatteryState,
-  BatteryStateFlagsValues,
-  type BatteryStateFlags,
-};
-
-export { Rule, Profile };
-
+// Colors
+export { default as Color } from "./color/Color";
 export * from "./color/colorUtils";
 export * from "./color/color32Utils";
 export * from "./color/gammaUtils";
+
+// Other
+export { default as Constants } from "./animations/Constants";
+export { default as DataSet } from "./DataSet";
