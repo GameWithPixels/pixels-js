9ac56c5e585a09ca53cec88b219b3f0f98af7944
Olivier Basille <obasille@hotmail.com>
2023-01-23 14:37:53 +0100

    fix(rn-pixels-components): fix toMask() for values >= 31

--- packages/react-native-pixels-components/src/faceMaskUtils.ts
+++ packages/react-native-pixels-components/src/faceMaskUtils.ts
@@ -1,10 +1,9 @@
 export function bitsToIndices(value: number): number[] {
   const indices: number[] = [];
 
-  // Convert value into binary
-  // We use a string because of the limitation of JS bits operators
+  // Convert value into binary: we use a string because of issues with
+  // JS bits operators (ex: 0x80000000 >> 1 => -1073741824 )
   let bits = value.toString(2);
-
   let index = 0;
   while (bits.length) {
     if (bits[bits.length - 1] === "1") {
@@ -17,7 +16,9 @@ export function bitsToIndices(value: number): number[] {
 }
 
 export function toMask(index: number): number {
-  return 1 << index;
+  // Use pow() function rather than bit shifting operator because of issues
+  // with the later (ex: 1 << 31 => -2147483648)
+  return Math.pow(2, index);
 }
 
 export function combine(values: number[]): number {
