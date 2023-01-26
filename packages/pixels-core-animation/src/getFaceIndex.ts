c8ca4cef7773771538783f771d837cbb5c7373d5
Olivier Basille <obasille@hotmail.com>
2023-01-26 13:00:13 +0100

    feat(px-core-anim): add getFaceMask() and export getFaceIndex()

--- packages/pixels-core-animation/src/getFaceIndex.ts
+++ packages/pixels-core-animation/src/getFaceIndex.ts
@@ -1,12 +1,12 @@
-const faceIndices = [
+const d20FaceIndices = [
   17, 1, 19, 13, 3, 10, 8, 5, 15, 7, 9, 11, 14, 4, 12, 0, 18, 2, 16, 6,
 ];
 
 /**
- * Returns the face index (also the LED index) for a give face value.
+ * Returns the face index (also the LED index) for a given die face value.
  * @param faceValue A face value.
  * @returns The corresponding face index.
  */
 export default function (faceValue: number): number {
-  return faceIndices[Math.floor(faceValue)];
+  return d20FaceIndices[Math.floor(faceValue)];
 }
