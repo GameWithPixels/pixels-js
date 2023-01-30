c9e315445ea3fb9caf02f2a2c340a347fffbc036
Olivier Basille <obasille@hotmail.com>
2023-01-30 11:26:16 +0100

    feat(pixels-app): add bitIndexToFlag() to bitMasksUtils

--- packages/react-native-pixels-components/src/bitMasksUtils.ts
+++ packages/react-native-pixels-components/src/bitMasksUtils.ts
@@ -23,8 +23,9 @@ export function bitsToIndices(value: number): number[] {
 }
 
 /**
- * Combine the given values into a single one by OR-ing them.
+ * Combines the given values into a single one by OR-ing them.
  * This is most useful when combining flags.
+ * @remarks Because of the limitations of the OR operator, the given flags must be less than 2^31.
  * @param flags List of numbers (flags) to combine.
  * @returns The combined value.
  */
@@ -36,4 +37,19 @@ export function combineBits(flags: number[]): number {
     );
     return prev | cur;
   });
+}
+
+/**
+ * Converts a given bit index to the corresponding flag value (which is 2^bitIndex).
+ * @remarks Because the value is returned as a 64 bits floating point number with a 52 bits mantissa,
+ * the given bit index must be less than 53.
+ * @param bitIndex A bit index.
+ * @returns The flag value for the given bit index.
+ */
+export function bitIndexToFlag(bitIndex: number) {
+  assert(
+    bitIndex < 53,
+    "Bit index greater than maximum precision of 64 bits floating-point numbers (52 bits mantissa)"
+  );
+  return Math.pow(2, bitIndex);
 }
