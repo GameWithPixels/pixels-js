0d226bc9aa540a5dc173d57bf3848fbde54319ae
Olivier Basille <obasille@hotmail.com>
2023-01-30 10:45:54 +0100

    feat(pixels-app): add keyValueUtils and add comments to bitMasksUtils functions

--- packages/react-native-pixels-components/src/bitMasksUtils.ts
+++ packages/react-native-pixels-components/src/bitMasksUtils.ts
@@ -1,8 +1,15 @@
+import assert from "../../pixels-core-utils/src/assert";
+
+/**
+ * Returns the indices of the "on" bits of the given value.
+ * @param value  The value to use.
+ * @returns The indices of the "on" bits.
+ */
 export function bitsToIndices(value: number): number[] {
   const indices: number[] = [];
 
-  // Convert value into binary: we use a string because of issues with
-  // JS bits operators (ex: 0x80000000 >> 1 => -1073741824 )
+  // Convert value to binary format: we use a string because of the limitations
+  // with JS bits operators (ex: 0x80000000 >> 1 => -1073741824 )
   let bits = value.toString(2);
   let index = 0;
   while (bits.length) {
@@ -15,6 +22,18 @@ export function bitsToIndices(value: number): number[] {
   return indices;
 }
 
-export function combineBits(bitMasks: number[]): number {
-  return bitMasks.reduce((prev, cur) => prev + cur);
+/**
+ * Combine the given values into a single one by OR-ing them.
+ * This is most useful when combining flags.
+ * @param flags List of numbers (flags) to combine.
+ * @returns The combined value.
+ */
+export function combineBits(flags: number[]): number {
+  return flags.reduce((prev, cur) => {
+    assert(
+      cur < 0x80000000,
+      "Flag value greater or equal to 2^31, can't combine using OR operator"
+    );
+    return prev | cur;
+  });
 }
