e1d864957a8261f54d71b86174cf1b43fdce558b
Olivier Basille <obasille@hotmail.com>
2023-02-02 15:04:50 +0100

    refactor(re-pixels-connect): remove duplicated props from ScannedPixel that are already declared in IPixel

--- packages/react-native-pixels-connect/src/ScannedPixel.ts
+++ packages/react-native-pixels-connect/src/ScannedPixel.ts
@@ -1,20 +1,5 @@
-import type {
-  IPixel,
-  PixelDesignAndColorNames,
-  PixelRollStateNames,
-} from "@systemic-games/pixels-core-connect";
+import type { IPixel } from "@systemic-games/pixels-core-connect";
 
 export default interface ScannedPixel extends IPixel {
-  readonly systemId: string;
-  readonly pixelId: number;
-  readonly address: number;
-  readonly name: string;
-  readonly ledCount: number;
-  readonly designAndColor: PixelDesignAndColorNames;
-  readonly firmwareDate: Date;
-  readonly rssi: number;
-  readonly batteryLevel: number; // Percentage
-  readonly isCharging: boolean;
-  readonly rollState: PixelRollStateNames;
-  readonly currentFace: number; // Face value (not index)
+  readonly address: number; // Not available on iOS
 }
