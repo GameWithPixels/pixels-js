303cbdc5111806f3d9933b5a14e7c97cb3ae3828
Olivier Basille <obasille@hotmail.com>
2023-02-02 21:20:27 +0100

    feat(px-react): add parameter to usePixelConnect() to directly pass a Pixel instance + usePixel() fix

--- packages/pixels-react/src/usePixelConnect.ts
+++ packages/pixels-react/src/usePixelConnect.ts
@@ -1,6 +1,6 @@
 import { Pixel, PixelStatus } from "@systemic-games/pixels-core-connect";
 import { assertNever } from "@systemic-games/pixels-core-utils";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import usePixelStatus from "./usePixelStatus";
 
@@ -19,14 +19,16 @@ import usePixelStatus from "./usePixelStatus";
  *          dispatch function. Connecting to another Pixel using the same dispatch
  *          will first disconnect from the previous Pixel.
  */
-export default function (): [
+export default function (
+  pixel?: Pixel
+): [
   PixelStatus | undefined,
   Pixel | undefined,
   (action: "connect" | "disconnect", pixel?: Pixel) => void,
   Error?
 ] {
   const [lastError, setLastError] = useState<Error>();
-  const pixelRef = useRef<Pixel>();
+  const pixelRef = useRef<Pixel | undefined>(pixel);
 
   // Create the dispatch function
   const dispatch = useCallback(
@@ -64,6 +66,18 @@ export default function (): [
     []
   );
 
-  const pixel = pixelRef.current;
-  return [usePixelStatus(pixel), pixel, dispatch, lastError];
+  // Disconnect on unmount and when given a different Pixel
+  useEffect(() => {
+    if (pixel) {
+      dispatch("connect", pixel);
+    }
+    return () => dispatch("disconnect");
+  }, [dispatch, pixel]);
+
+  return [
+    usePixelStatus(pixelRef.current),
+    pixelRef.current,
+    dispatch,
+    lastError,
+  ];
 }
