6f1c1ec9aaaf822cc1285c0e475742eab91601f2
Olivier Basille <obasille@hotmail.com>
2023-04-03 17:37:09 +0200

    chore(toolbox): add react-native-paper

--- apps/pixels-toolbox/babel.config.js
+++ apps/pixels-toolbox/babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
     plugins: [
       [
         "babel-plugin-root-import",
