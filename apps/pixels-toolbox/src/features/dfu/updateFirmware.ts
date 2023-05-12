40532ec4bdb1fa08fa76503fce2999a9354c4f37
Olivier Basille <obasille@hotmail.com>
2023-05-12 16:19:04 +0200

    fix(toolbox): skip sending "dfuComplete" event after flashing bootloader when the firmware will be flashed next

--- apps/pixels-toolbox/src/features/dfu/updateFirmware.ts
+++ apps/pixels-toolbox/src/features/dfu/updateFirmware.ts
@@ -14,21 +14,29 @@ export default async function (
   setDfuState?: (state: DfuState) => void,
   setDfuProgress?: (progress: number) => void
 ): Promise<void> {
-  // Prepare DFU options
-  const dfuOptions = {
-    retries: 3,
-    dfuStateListener: ({ state }: DfuStateEvent) => {
-      console.log(`DFU state changed: ${state}`);
-      setDfuState?.(state);
-    },
-    dfuProgressListener: ({ percent }: DfuProgressEvent) => {
-      setDfuProgress?.(percent);
-    },
-  };
-
   const addrStr = pixelAddress.toString(16);
   const hasFirmware = !!firmwarePath?.length;
   const hasBootloader = !!bootloaderPath?.length;
+
+  // Prepare DFU options
+  const dfuCount = (hasBootloader ? 1 : 0) + (hasFirmware ? 1 : 0);
+  let pendingDfuCount = dfuCount;
+  const dfuOptions = {
+    retries: 3,
+    dfuStateListener: ({ state }: DfuStateEvent) => {
+      const index = dfuCount - pendingDfuCount;
+      console.log(`DFU state changed: ${state} (${index} of ${dfuCount})`);
+      if (state !== "dfuCompleted" || pendingDfuCount <= 0) {
+        setDfuState?.(state);
+      }
+    },
+    dfuProgressListener: ({ percent }: DfuProgressEvent) => {
+      const p =
+        ((dfuCount - pendingDfuCount - 1) / dfuCount) * 100 +
+        percent / (pendingDfuCount + 1);
+      setDfuProgress?.(p);
+    },
+  };
 
   // Update bootloader
   if (hasBootloader) {
@@ -36,6 +44,7 @@ export default async function (
       console.log(
         `Starting DFU for device ${addrStr} with bootloader ${bootloaderPath}`
       );
+      pendingDfuCount -= 1;
       await startDfu(pixelAddress, bootloaderPath, dfuOptions);
     } catch (error: any) {
       if (error instanceof DfuFirmwareVersionFailureError) {
@@ -59,6 +68,7 @@ export default async function (
       console.log(
         `Starting DFU for device ${addrStr} with firmware ${firmwarePath}`
       );
+      pendingDfuCount -= 1;
       const addr = pixelAddress + (hasBootloader ? 1 : 0);
       await startDfu(addr, firmwarePath, dfuOptions);
     } catch (error) {
