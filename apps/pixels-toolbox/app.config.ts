d56ee652424a142024fb7b4c076ea78295ba5f03
Jean Simonet <frogie@gmail.com>
2023-05-17 17:01:29 -0400

    chore(toolbox): update version and new firmware files

--- apps/pixels-toolbox/app.config.ts
+++ apps/pixels-toolbox/app.config.ts
@@ -9,7 +9,7 @@ const config = {
     slug: prod ? "pixels-toolbox" : "pixels-toolbox-dev",
     owner: "gamewithpixels",
     runtimeVersion: "48.3",
-    version: "1.8.1",
+    version: "1.8.2",
     platforms: ["ios", "android"],
     orientation: "portrait",
     icon: prod ? "./assets/images/icon.png" : "./assets/images/icon-dev.png",
