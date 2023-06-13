a8cc12426d04466a9fe4144e2efa76143c7826d9
Jean Simonet <frogie@gmail.com>
2023-06-13 12:29:13 -0400

    chore(toolbox): updated firmware and toolbox code to improve BT connection

--- apps/pixels-toolbox/app.config.ts
+++ apps/pixels-toolbox/app.config.ts
@@ -9,7 +9,7 @@ const config = {
     slug: prod ? "pixels-toolbox" : "pixels-toolbox-dev",
     owner: "gamewithpixels",
     runtimeVersion: "48.4",
-    version: "1.9.5",
+    version: "1.9.7",
     platforms: ["ios", "android"],
     orientation: "portrait",
     icon: prod ? "./assets/images/icon.png" : "./assets/images/icon-dev.png",
