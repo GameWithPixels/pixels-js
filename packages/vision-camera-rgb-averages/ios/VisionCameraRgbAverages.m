6a8a909e556f7dcfa1ce41e0815c67c2c86f74df
Olivier Basille <obasille@hotmail.com>
2023-07-05 16:25:22 +0200

    feat(rgb-avg): add option to use vector processor on iOS to convert image to RGB + fix actual convertion

--- packages/vision-camera-rgb-averages/ios/VisionCameraRgbAverages.m
+++ packages/vision-camera-rgb-averages/ios/VisionCameraRgbAverages.m
@@ -1,13 +1,14 @@
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/Frame.h>
 #import <Endian.h>
+@import Accelerate;
 
 @interface QRCodeFrameProcessorPlugin : NSObject
 @end
 
 @implementation QRCodeFrameProcessorPlugin
 
-static id processImage(void *baseAddress, CVImageBufferRef imageBuffer, int subSamplingX, int subSamplingY, bool writeRbgImager, bool writeYuvPlanes)
+static id processImage(void *baseAddress, CVImageBufferRef imageBuffer, int subSamplingX, int subSamplingY, bool useVectorProc)
 {
     NSUInteger planeCount = CVPixelBufferGetPlaneCount(imageBuffer);
     OSType type = CVPixelBufferGetPixelFormatType(imageBuffer);
@@ -24,92 +25,168 @@ static id processImage(void *baseAddress, CVImageBufferRef imageBuffer, int subS
     }
     if (subSamplingX <= 0)
     {
-        return @"subSampling value must be 1 or greater";
+        return @"Invalid parameter, subSamplingX must be 1 or greater";
     }
     if (subSamplingY <= 0)
     {
-        return @"subSampling value must be 1 or greater";
+        return @"Invalid parameter, subSamplingY must be 1 or greater";
     }
 
     // Track how long it takes to process the image
     NSDate *startTime = [NSDate date];
-    
-    // Get image info
-    NSUInteger width = CVPixelBufferGetWidth(imageBuffer);
-    NSUInteger height = CVPixelBufferGetHeight(imageBuffer);
-    NSUInteger plane0Width = CVPixelBufferGetWidthOfPlane(imageBuffer, 0);
-    NSUInteger plane0BytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0);
-    NSUInteger plane1BytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 1);
 
-    uint8_t *yBuffer = baseAddress;
-    uint8_t *uBuffer = baseAddress + plane0Width;
-    uint8_t *vBuffer = baseAddress + plane0Width + 1;
-
-    NSUInteger yRowStride = plane0BytesPerRow * subSamplingY;
-    NSUInteger yPixelStride = subSamplingX;
-    NSUInteger uvRowStride = plane1BytesPerRow * subSamplingY / 2;
-    NSUInteger uvPixelStride = subSamplingX / 2;
-
-    // U/V Values are sub-sampled i.e. each pixel in U/V channel in a
-    // YUV_420 image act as chroma value for 4 neighboring pixels
-    NSUInteger uvIndex = 0, uvIndexRowStart = 0;
-
-    NSUInteger pixelsCount = (width / subSamplingX) * (height / subSamplingY);
-    //final byte[] argb = writeRgbImage ? getArgbBuffer(pixelsCount * 4) : null;
-    NSUInteger argbIndex = 0;
+    // Get image size
+    const NSUInteger width = CVPixelBufferGetWidth(imageBuffer);
+    const NSUInteger height = CVPixelBufferGetHeight(imageBuffer);
 
     double rSum = 0, gSum = 0, bSum = 0;
-    NSUInteger yIndex = 0, yIndexRowStart = 0;
 
-    bool isSubSamplingX = subSamplingX > 1;
-    bool isSubSamplingY = subSamplingY > 1;
-    for (NSUInteger y = 0; y < height; y += subSamplingY)
+    if (useVectorProc)
     {
-        for (NSUInteger x = 0; x < width; x += subSamplingX)
+        // Convert to RGB buffer
+        static uint32_t *rgbBuffer = NULL;
+        static vImage_YpCbCrPixelRange pixelRange;
+        static vImage_YpCbCrToARGB info;
+        vImage_Error convError = kvImageNoError;
+        if (!rgbBuffer)
         {
-            int yValue = yBuffer[yIndex] & 0xFF;
+            // https://developer.apple.com/documentation/accelerate/vimage_ypcbcrpixelrange?language=objc
+            // video range 8-bit, clamped to video range
+            pixelRange.Yp_bias = 16;
+            pixelRange.CbCr_bias = 128;
+            pixelRange.YpRangeMax = 265;
+            pixelRange.CbCrRangeMax = 240;
+            pixelRange.YpMax = 235;
+            pixelRange.YpMin = 16;
+            pixelRange.CbCrMax = 240;
+            pixelRange.CbCrMin = 16;
             
-            // U/V values ideally fall under [-0.5, 0.5] range. To fit them into
-            // [0, 255] range they are scaled up and centered to 128.
-            // Operation below brings U/V values to [-128, 127].
-            int uValue = (uBuffer[uvIndex] & 0xFF) - 128;
-            int vValue = (vBuffer[uvIndex] & 0xFF) - 128;
-
-            // https://en.wikipedia.org/wiki/YUV#Y%E2%80%B2UV420sp_(NV21)_to_RGB_conversion_(Android)
-            // Fast integer computing with a small approximation
-            int r = yValue + ((351 * vValue) >> 8);
-            int g = yValue - ((179 * vValue + 86 * uValue) >> 8);
-            int b = yValue + ((443 * uValue) >> 8);
-//                int r = (int)(0.5f + yValue + 1.370705f * vValue);
-//                int g = (int)(0.5f + yValue - 0.698001f * vValue - 0.337633f * uValue);
-//                int b = (int)(0.5f + yValue + 1.732446f * uValue);
-
-            // Use raw values (not clamped to [0, 255])
-            rSum += r;
-            gSum += g;
-            bSum += b;
-
-            // Next pixel, taking sub-sampling into account
-            yIndex += yPixelStride;
-            if (isSubSamplingX || (x & 1) != 0)
+            convError = vImageConvert_YpCbCrToARGB_GenerateConversion(kvImage_YpCbCrToARGBMatrix_ITU_R_601_4, &pixelRange,
+                                                                      &info, kvImage422CbYpCrYp8, kvImageARGB8888, kvImageNoFlags);
+            
+            if (convError == kvImageNoError)
             {
-                uvIndex += uvPixelStride;
+                rgbBuffer = malloc(width * height * 4);
             }
         }
-
-        // Next line, taking sub-sampling into account
-        yIndexRowStart += yRowStride;
-        yIndex = yIndexRowStart;
-        if (isSubSamplingY || (y & 1) != 0)
+        
+        if (convError == kvImageNoError)
         {
-            uvIndexRowStart += uvRowStride;
+            vImage_Buffer yImgBuffer;
+            yImgBuffer.data = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0);;
+            yImgBuffer.width = CVPixelBufferGetWidthOfPlane(imageBuffer, 0);
+            yImgBuffer.height = CVPixelBufferGetHeightOfPlane(imageBuffer, 0);
+            yImgBuffer.rowBytes = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0);
+            vImage_Buffer uvImgBuffer;
+            uvImgBuffer.data = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 1);;
+            uvImgBuffer.width = CVPixelBufferGetWidthOfPlane(imageBuffer, 1);
+            uvImgBuffer.height = CVPixelBufferGetHeightOfPlane(imageBuffer, 1);
+            uvImgBuffer.rowBytes = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 1);
+            vImage_Buffer rgbImgBuffer;
+            rgbImgBuffer.data = rgbBuffer;
+            rgbImgBuffer.width = width;
+            rgbImgBuffer.height = height;
+            rgbImgBuffer.rowBytes = 4 * width;
+            
+            convError = vImageConvert_420Yp8_CbCr8ToARGB8888(&yImgBuffer, &uvImgBuffer, &rgbImgBuffer, &info, nil, 0, kvImageNoFlags);
         }
-        uvIndex = uvIndexRowStart;
+        
+        if (convError == kvImageNoError)
+        {
+            uint32_t *rgbPtr = rgbBuffer; //rgbBuffer;
+            for (NSUInteger y = 0; y < height; y += subSamplingY)
+            {
+                for (NSUInteger x = 0; x < width; x += subSamplingX)
+                {
+                    const uint32_t argb = *rgbPtr;
+                    const int r = (argb & 0x0000ff00) >> 8;
+                    const int b = (argb & 0xff000000) >> 24;
+                    const int g = (argb & 0x00ff0000) >> 16;
+                    
+                    // Use raw values (not clamped to [0, 255])
+                    rSum += r;
+                    gSum += g;
+                    bSum += b;
+                    
+                    rgbPtr += subSamplingX;
+                }
+                
+                rgbPtr += (subSamplingY - 1) * width;
+            }
+        }
+        else
+        {
+            return @"Error converting image to RGB";
+        }
+    }
+    else
+    {
+        // Get image info
+        NSUInteger plane0BytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0);
+        NSUInteger plane1BytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 1);
+        
+        const uint8_t *yBuffer = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0);
+        const uint8_t *uBuffer = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 1);
+        const uint8_t *vBuffer = uBuffer + 1;
+        
+        const NSUInteger yRowStride = plane0BytesPerRow * subSamplingY;
+        const NSUInteger yPixelStride = subSamplingX;
+        const NSUInteger uvRowStride = plane1BytesPerRow * subSamplingY / 2;
+        const NSUInteger uvPixelStride = subSamplingX / 2;
+        
+        // U/V Values are sub-sampled i.e. each pixel in U/V channel in a
+        // YUV_420 image act as chroma value for 4 neighboring pixels
+        NSUInteger uvIndex = 0, uvIndexRowStart = 0;
+        NSUInteger yIndex = 0, yIndexRowStart = 0;
+        
+        const bool isSubSamplingX = subSamplingX > 1;
+        const bool isSubSamplingY = subSamplingY > 1;
+
+        for (NSUInteger y = 0; y < height; y += subSamplingY)
+        {
+            for (NSUInteger x = 0; x < width; x += subSamplingX)
+            {
+                int yValue = yBuffer[yIndex] & 0xFF;
+
+                // U/V values ideally fall under [-0.5, 0.5] range. To fit them into
+                // [0, 255] range they are scaled up and centered to 128.
+                // Operation below brings U/V values to [-128, 127].
+                const int uValue = (uBuffer[uvIndex] & 0xFF) - 128;
+                const int vValue = (vBuffer[uvIndex] & 0xFF) - 128;
+
+                // https://en.wikipedia.org/wiki/YUV#Y%E2%80%B2UV420sp_(NV21)_to_RGB_conversion_(Android)
+                // Fast integer computing with a small approximation
+                const int r = yValue + ((351 * vValue) >> 8);
+                const int g = yValue - ((179 * vValue + 86 * uValue) >> 8);
+                const int b = yValue + ((443 * uValue) >> 8);
+
+                // Use raw values (not clamped to [0, 255])
+                rSum += r;
+                gSum += g;
+                bSum += b;
+
+                // Next pixel, taking sub-sampling into account
+                yIndex += yPixelStride;
+                if (isSubSamplingX || (x & 1) != 0)
+                {
+                    uvIndex += uvPixelStride;
+                }
+            }
+
+            // Next line, taking sub-sampling into account
+            yIndexRowStart += yRowStride;
+            yIndex = yIndexRowStart;
+            if (isSubSamplingY || (y & 1) != 0)
+            {
+                uvIndexRowStart += uvRowStride;
+            }
+            uvIndex = uvIndexRowStart;
+        }
     }
 
-    NSDate *endTime = [NSDate date];
-    NSTimeInterval timestamp = [endTime timeIntervalSince1970];
-    NSTimeInterval duration = [endTime timeIntervalSinceDate:startTime];
+    const NSUInteger pixelsCount = (width / subSamplingX) * (height / subSamplingY);
+    const NSTimeInterval timestamp = [startTime timeIntervalSince1970];
+    const NSTimeInterval duration = [[NSDate date] timeIntervalSinceDate:startTime];
     return @{
         @"timestamp": [NSNumber numberWithDouble:1000 * timestamp],
         @"duration": [NSNumber numberWithDouble:1000 * duration],
@@ -119,9 +196,9 @@ static id processImage(void *baseAddress, CVImageBufferRef imageBuffer, int subS
 //        @"yPixelStride", yPixelStride);
 //        @"uvRowStride", uvRowStride);
 //        @"uvPixelStride", uvPixelStride);
-        @"redAverage": [NSNumber numberWithDouble:rSum / pixelsCount],
-        @"greenAverage": [NSNumber numberWithDouble:gSum / pixelsCount],
-        @"blueAverage": [NSNumber numberWithDouble:bSum / pixelsCount],
+        @"redAverage": [NSNumber numberWithDouble:round(rSum / pixelsCount)],
+        @"greenAverage": [NSNumber numberWithDouble:round(gSum / pixelsCount)],
+        @"blueAverage": [NSNumber numberWithDouble:round(bSum / pixelsCount)],
     };
 }
 
@@ -130,8 +207,7 @@ static inline id getImageRgbAverages(Frame* frame, NSArray* args)
     // Read parameters (must match processImage() parameters)
     int subSamplingX = 1;
     int subSamplingY = 1;
-    bool writeRbgImage = false;
-    bool writeYuvPlanes = false;
+    bool useVectorProc = false;
 
     int paramCounter = 0;
     for (NSObject *param in args)
@@ -150,16 +226,9 @@ static inline id getImageRgbAverages(Frame* frame, NSArray* args)
                     subSamplingY = num.intValue;
                 }
             }
-            else
+            else if (paramCounter == 2)
             {
-                if (paramCounter == 2)
-                {
-                    writeRbgImage = num.boolValue;
-                }
-                else //if (paramCounter == 3) {
-                {
-                    writeYuvPlanes = num.boolValue;
-                }
+                useVectorProc = num.boolValue;
             }
         }
         ++paramCounter;
@@ -167,7 +236,7 @@ static inline id getImageRgbAverages(Frame* frame, NSArray* args)
 
 
     CMSampleBufferRef buffer = frame.buffer;
-    UIImageOrientation orientation = frame.orientation;
+    // UIImageOrientation orientation = frame.orientation;
     CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(buffer);
     
     id result = nil;
@@ -176,14 +245,25 @@ static inline id getImageRgbAverages(Frame* frame, NSArray* args)
         CVPixelBufferLockBaseAddress(imageBuffer, 0);
         
         void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
-
         if (baseAddress)
         {
-//            NSData *data = [NSData dataWithBytes:baseAddress length:bytesPerRow * height];
-            result = processImage(baseAddress, imageBuffer, subSamplingX, subSamplingY, writeRbgImage, writeYuvPlanes);
+            const bool invXY = CVPixelBufferGetWidth(imageBuffer) < CVPixelBufferGetHeight(imageBuffer);
+            result = processImage(baseAddress,
+                                  imageBuffer,
+                                  invXY ? subSamplingY : subSamplingX,
+                                  invXY ? subSamplingX : subSamplingY,
+                                  useVectorProc);
         }
-        
+        else
+        {
+            result = @"Error getting image base address";
+        }
+
         CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+    }
+    else
+    {
+        result = @"Error getting image buffer";
     }
 
     return result;
--- packages/vision-camera-rgb-averages/src/index.ts
+++ packages/vision-camera-rgb-averages/src/index.ts
@@ -1,26 +1,53 @@
+import { Platform } from "react-native";
 import type { Frame } from "react-native-vision-camera";
 
 declare let _WORKLET: true | undefined;
 
+/**
+ * Optional parameters for {@link getImageRgbAverages}.
+ */
 export interface ImageRgbAveragesOptions {
+  /** Sub-sampling along the long dimension for computing average. */
   subSamplingX?: number;
+  /** Sub-sampling along the short dimension for computing average. */
   subSamplingY?: number;
+  /** Use vector processor to convert image to RGB. */
+  useVectorProc?: boolean; // iOS only
+  /** Output image to file. Android only. */
   writeImage?: boolean;
+  /** Output YUV planes to files. Android only. */
   writePlanes?: boolean;
 }
 
+/**
+ * Type of return value of {@link getImageRgbAverages}.
+ */
 export interface ImageRgbAverages {
+  /** Timestamp of when the frame processor started to work on the frame. */
   timestamp: number;
+  /** Time im ms it took the frame processor to process the frame. */
   duration: number;
+  /** Width of processed frame. */
   width: number;
+  /** Height of processed frame. */
   height: number;
+  /** Average amount of red in the frame. */
   redAverage: number;
+  /** Average amount of green in the frame. */
   greenAverage: number;
+  /** Average amount of blue in the frame. */
   blueAverage: number;
 }
 
-// Frame Processor that computes the image red, green and blue average
-// Throws an exception if invalid arguments are given
+/**
+ * Frame Processor that the image red, green and blue average for the
+ * entire given frame.
+ * @param frame The frame to use.
+ * @param opt Optional parameters, see {@link ImageRgbAveragesOptions}.
+ * @returns An object with red, green and blue averages,
+ *          see {@link ImageRgbAveragesOptions}.
+ * @throws Throws an exception when given invalid parameters.
+ */
 export function getImageRgbAverages(
   frame: Frame,
   opt?: ImageRgbAveragesOptions
@@ -31,15 +58,27 @@ export function getImageRgbAverages(
       "getImageRgbAverages() must be called from a frame processor!"
     );
 
-  // @ts-expect-error because this function is dynamically injected by VisionCamera
-  // eslint-disable-next-line no-undef
-  const result = __getImageRgbAverages(
-    frame,
-    opt?.subSamplingX ?? 1,
-    opt?.subSamplingY ?? 1,
-    opt?.writeImage ?? false,
-    opt?.writePlanes ?? false
-  );
+  const result =
+    Platform.OS === "android"
+      ? // @ts-expect-error because this function is dynamically injected by VisionCamera
+        __getImageRgbAverages(
+          frame,
+          opt?.subSamplingX ?? 1,
+          opt?.subSamplingY ?? 1,
+          opt?.writeImage ?? false,
+          opt?.writePlanes ?? false
+        )
+      : // @ts-expect-error because this function is dynamically injected by VisionCamera
+        __getImageRgbAverages(
+          frame,
+          opt?.subSamplingX ?? 1,
+          opt?.subSamplingY ?? 1,
+          opt?.useVectorProc ?? false
+        );
 
-  return result as ImageRgbAverages;
+  if (typeof result === "string") {
+    throw new Error(result);
+  } else {
+    return result as ImageRgbAverages;
+  }
 }
