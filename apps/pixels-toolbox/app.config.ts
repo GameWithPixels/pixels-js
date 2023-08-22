ecc1b693e6f5e11d6cb7633b9ea2c498f0dd8635
Olivier Basille <obasille@hotmail.com>
2023-08-23 00:12:28 +0200

    chore(toolbox): release 3.0 with firmware 2023-08-23

--- apps/pixels-toolbox/app.config.ts
+++ apps/pixels-toolbox/app.config.ts
@@ -8,8 +8,8 @@ const config = {
     name: prod ? "Pixels Toolbox" : "Dev Toolbox",
     slug: prod ? "pixels-toolbox" : "pixels-toolbox-dev",
     owner: "gamewithpixels",
-    runtimeVersion: "48.6",
-    version: "2.2",
+    runtimeVersion: "48.7",
+    version: "3.0.0", // Version number must have 3 parts
     platforms: ["ios", "android"],
     orientation: "portrait",
     icon: prod ? "./assets/images/icon.png" : "./assets/images/icon-dev.png",
