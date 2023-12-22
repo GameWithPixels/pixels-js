c9792d171e1f93ceb532477c23ead9e09efe8060
Jean Simonet <frogie@gmail.com>
2023-12-22 14:41:38 -0500

    feat(toolbox): build default profile programmatically, just like in the firmware

--- apps/pixels-toolbox/src/features/pixels/getDefaultProfile.ts
+++ apps/pixels-toolbox/src/features/pixels/getDefaultProfile.ts
@@ -1,26 +1,268 @@
-import { PixelDieType } from "@systemic-games/pixels-core-connect";
+import { PixelDieType, DiceUtils } from "@systemic-games/pixels-core-connect";
 import {
   DataSet,
-  Json,
   createDataSetForProfile,
-  loadAppDataSet,
+  EditActionPlayAnimation,
+  EditAnimationRainbow,
+  EditConditionFaceCompare,
+  EditConditionHelloGoodbye,
+  EditProfile,
+  EditRule,
+  EditConditionConnectionState,
+  EditAnimationSimple,
+  EditConditionRolling,
+  EditColor,
+  EditConditionBatteryState,
 } from "@systemic-games/pixels-edit-animation";
-
-function getFirstProfileDataSet(jsonData: Json.DataSet): DataSet {
-  const defaultProfileAppDataSet = loadAppDataSet(jsonData);
-  return createDataSetForProfile(
-    defaultProfileAppDataSet.profiles[0],
-    defaultProfileAppDataSet.defaultProfile
-  ).toDataSet();
-}
+import {
+  Color,
+  FaceCompareFlagsValues,
+  HelloGoodbyeFlagsValues,
+  Constants,
+  ConnectionStateFlagsValues,
+  AnimationFlagsValues,
+  getFaceMask,
+  BatteryStateFlagsValues,
+} from "@systemic-games/react-native-pixels-connect";
 
 export function getDefaultProfile(dieType: PixelDieType): DataSet {
-  // Update the project tsconfig.json after adding a new profile JSON file to the build
-  switch (dieType) {
-    case "d20":
-    default:
-      return getFirstProfileDataSet(
-        require(`!/profiles/default-profile-d20.json`)
-      );
-  }
+  const profile = new EditProfile();
+  profile.name = "fixedRainbow";
+  // Hello
+  profile.rules.push(
+    new EditRule(
+      new EditConditionHelloGoodbye({ flags: HelloGoodbyeFlagsValues.hello }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationRainbow({
+              animFlags:
+                AnimationFlagsValues.traveling |
+                AnimationFlagsValues.useLedIndices,
+              duration: 2.0,
+              faces: Constants.faceMaskAll,
+              count: 2,
+              fade: 200 / 255,
+              intensity: 0x80 / 255,
+              cycles: 1,
+            }),
+            face: Constants.currentFaceIndex,
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+  // Connection
+  profile.rules.push(
+    new EditRule(
+      new EditConditionConnectionState({
+        flags:
+          ConnectionStateFlagsValues.connected |
+          ConnectionStateFlagsValues.disconnected,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 2,
+              duration: 1,
+              color: Color.blue,
+              faces: Constants.faceMaskAll,
+            }),
+            face: Constants.currentFaceIndex,
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Rolling
+  profile.rules.push(
+    new EditRule(new EditConditionRolling({ recheckAfter: 0.5 }), {
+      actions: [
+        new EditActionPlayAnimation({
+          animation: new EditAnimationSimple({
+            count: 1,
+            duration: 0.1,
+            color: new EditColor("face"),
+            faces: getFaceMask(DiceUtils.getTopFace(dieType), dieType),
+          }),
+          face: Constants.currentFaceIndex,
+          loopCount: 1,
+        }),
+      ],
+    })
+  );
+
+  // OnFace
+  profile.rules.push(
+    new EditRule(
+      new EditConditionFaceCompare({
+        flags: FaceCompareFlagsValues.equal | FaceCompareFlagsValues.greater,
+        face: 0,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 1,
+              duration: 3,
+              color: new EditColor("face"),
+              faces: Constants.faceMaskAll,
+            }),
+            face: Constants.currentFaceIndex,
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Low Batt
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.low,
+        recheckAfter: 30,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 3,
+              duration: 1.5,
+              color: Color.red,
+              faces: Constants.faceMaskAll,
+            }),
+            face: Constants.currentFaceIndex,
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Charging
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.charging,
+        recheckAfter: 5,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 1,
+              duration: 3,
+              color: Color.red,
+              faces: getFaceMask(DiceUtils.getTopFace(dieType), dieType),
+            }),
+            face: DiceUtils.getTopFace(dieType),
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Fully Charged
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.done,
+        recheckAfter: 5,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 1,
+              duration: 3,
+              color: Color.green,
+              faces: getFaceMask(DiceUtils.getTopFace(dieType), dieType),
+            }),
+            face: DiceUtils.getTopFace(dieType),
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Bad Charging
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.badCharging,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 10,
+              duration: 2,
+              color: Color.red,
+              faces: Constants.faceMaskAll,
+            }),
+            face: DiceUtils.getTopFace(dieType),
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Charging Error
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.error,
+        recheckAfter: 1.5,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 1,
+              duration: 1,
+              color: Color.yellow,
+              faces: getFaceMask(DiceUtils.getTopFace(dieType), dieType),
+            }),
+            face: DiceUtils.getTopFace(dieType),
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  // Charging Error
+  profile.rules.push(
+    new EditRule(
+      new EditConditionBatteryState({
+        flags: BatteryStateFlagsValues.error,
+        recheckAfter: 1.5,
+      }),
+      {
+        actions: [
+          new EditActionPlayAnimation({
+            animation: new EditAnimationSimple({
+              count: 1,
+              duration: 1,
+              color: Color.yellow,
+              faces: getFaceMask(DiceUtils.getTopFace(dieType), dieType),
+            }),
+            face: DiceUtils.getTopFace(dieType),
+            loopCount: 1,
+          }),
+        ],
+      }
+    )
+  );
+
+  const dataSet: DataSet = createDataSetForProfile(profile).toDataSet();
+  return dataSet;
 }
