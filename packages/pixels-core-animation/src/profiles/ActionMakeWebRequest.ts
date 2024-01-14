a7e4af547fb8ad5f70e77e38fa0c0f9524143e0d
Olivier Basille <obasille@hotmail.com>
2024-01-14 16:37:39 +0100

    fix(core-anim): add 1 byte after action to "run on device" actions for FW compat

--- packages/pixels-core-animation/src/profiles/ActionMakeWebRequest.ts
+++ packages/pixels-core-animation/src/profiles/ActionMakeWebRequest.ts
@@ -11,6 +11,9 @@ export default class ActionMakeWebRequest implements Action {
   @serializable(1)
   type: number = ActionTypeValues.makeWebRequest;
 
+  @serializable(1)
+  readonly _unused = 0;
+
   @serializable(2)
   actionId = 0;
 }
