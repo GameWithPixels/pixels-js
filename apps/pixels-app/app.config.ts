21b077a3677d3c5fdd983ee3243fd325d1406886
Olivier Basille <obasille@hotmail.com>
2024-01-16 23:09:20 +0100

    chore(pixels-app): v2.0.8

--- apps/pixels-app/app.config.ts
+++ apps/pixels-app/app.config.ts
@@ -9,7 +9,7 @@ const config = {
     slug: prod ? "pixels-app" : "pixels-app-dev",
     owner: "gamewithpixels",
     runtimeVersion: "49.2", // Major is Expo version, minor is native code revision
-    version: "2.0.7", // Version number must have 3 parts
+    version: "2.0.8", // Version number must have 3 parts
     platforms: ["ios", "android"],
     orientation: "portrait",
     icon: prod ? "./assets/images/icon.png" : "./assets/images/icon-dev.png",
@@ -29,7 +29,7 @@ const config = {
       bundleIdentifier: prod
         ? "com.systemic-games.pixels"
         : "com.systemicgames.pixelsappdev",
-      buildNumber: "2007",
+      buildNumber: "2008",
       supportsTablet: false,
       infoPlist: {
         NSCameraUsageDescription: "Allow $(PRODUCT_NAME) to access your camera",
@@ -46,7 +46,7 @@ const config = {
       package: prod
         ? "com.SystemicGames.Pixels"
         : "com.systemicgames.pixelsappdev",
-      versionCode: 2007,
+      versionCode: 2008,
       adaptiveIcon: {
         foregroundImage: prod
           ? "./assets/images/adaptive-icon.png"
