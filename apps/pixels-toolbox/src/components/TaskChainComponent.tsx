ec770e11cdb8e1fffa0cbaffe6efdd4da8497c6f
Olivier Basille <obasille@hotmail.com>
2024-01-29 14:28:23 +0100

    fix(toolbox): in validation don't display error message when there is none (i.e. task was canceled)

--- apps/pixels-toolbox/src/components/TaskChainComponent.tsx
+++ apps/pixels-toolbox/src/components/TaskChainComponent.tsx
@@ -21,24 +21,24 @@ export function TaskChainComponent({
   const taskStatus = taskChain.status;
   const { colors } = useTheme();
   const { t } = useTranslation();
-  const getMessage = (error?: Error): string => {
-    return error instanceof LocalizedError
+  const getMessage = (error?: Error): string =>
+    error instanceof LocalizedError
       ? (error as LocalizedError).toLocalizedString(t)
       : error instanceof TaskCanceledError
       ? (error as TaskCanceledError).userMessage ?? t("canceled")
-      : error?.message ?? String(error);
-  };
+      : error?.message ?? (error ? String(error) : "");
   return (
     <Card>
       <Card.Content>
         <TaskContainer title={title} taskStatus={taskStatus}>
-          {taskStatus === "faulted" || taskStatus === "canceled" ? (
-            <Text variant="titleLarge" style={{ color: colors.error }}>
-              {getMessage(taskChain.lastError)}
-            </Text>
-          ) : (
-            taskStatus !== "succeeded" && taskChain.render()
-          )}
+          {taskStatus === "faulted" || taskStatus === "canceled"
+            ? !!taskChain.lastError && (
+                // Show error message if any
+                <Text variant="titleLarge" style={{ color: colors.error }}>
+                  {getMessage(taskChain.lastError)}
+                </Text>
+              )
+            : taskStatus !== "succeeded" && taskChain.render()}
         </TaskContainer>
         {children}
       </Card.Content>
