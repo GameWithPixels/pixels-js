5a11ccb55ee7cb4cbc4a730da107c6f1adbc6696
Olivier Basille <obasille@hotmail.com>
2024-02-02 13:36:42 +0100

    fix(edit-anim): add EditAnimationSequenceItem to module exports

--- packages/pixels-edit-animation/src/edit/index.ts
+++ packages/pixels-edit-animation/src/edit/index.ts
@@ -17,6 +17,7 @@ export { default as EditAnimationRainbow } from "./EditAnimationRainbow";
 export { default as EditAnimationSimple } from "./EditAnimationSimple";
 export { default as EditAnimationNormals } from "./EditAnimationNormals";
 export { default as EditAnimationSequence } from "./EditAnimationSequence";
+export { default as EditAnimationSequenceItem } from "./EditAnimationSequenceItem";
 export { default as EditAudioClip } from "./EditAudioClip";
 export { default as EditColor } from "./EditColor";
 export { default as EditCondition } from "./EditCondition";
