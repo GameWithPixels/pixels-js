f01c37cd60cbda0f6d4c83c160e8d864c4912603
Olivier Basille <obasille@hotmail.com>
2024-02-19 17:50:01 +0100

    feat(toolbox): blink PixelInfoCard when die is rolling with a brighter flash when on face

--- apps/pixels-toolbox/src/components/PixelInfoCard.tsx
+++ apps/pixels-toolbox/src/components/PixelInfoCard.tsx
@@ -11,6 +11,15 @@ import React from "react";
 import { TFunction, useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { Card, Text } from "react-native-paper";
+import Animated, {
+  Easing,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 import { AppStyles } from "~/AppStyles";
 import { useForceUpdate } from "~/features/hooks/useForceUpdate";
@@ -163,6 +172,63 @@ function PixelMoreInfo(props: PixelAndTranslation) {
   );
 }
 
+function BlinkingCard({
+  children,
+  pixel,
+}: React.PropsWithChildren<Pick<PixelAndTranslation, "pixel">>) {
+  const isLoopingRef = React.useRef(false);
+  const animValue = useSharedValue(0);
+  const animStyle = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(
+      animValue.value,
+      [0, 1],
+      ["transparent", "dimgray"]
+    ),
+  }));
+  React.useEffect(() => {
+    const flash = (x0: number, x1: number) =>
+      withSequence(
+        withTiming(x0, {
+          duration: 600,
+          easing: Easing.out(Easing.ease),
+        }),
+        withTiming(x1, { duration: 300, easing: Easing.in(Easing.ease) })
+      );
+    const listener = () => {
+      if (pixel.rollState === "rolling" || pixel.rollState === "handling") {
+        if (!isLoopingRef.current) {
+          isLoopingRef.current = true;
+          animValue.value = withRepeat(flash(0.5, 0), -1);
+        }
+      } else {
+        isLoopingRef.current = false;
+        animValue.value = flash(1, 0);
+      }
+    };
+    pixel.addPropertyListener("rollState", listener);
+    return () => {
+      pixel.removePropertyListener("rollState", listener);
+    };
+  }, [animValue, pixel]);
+
+  return (
+    <Card style={{ overflow: "hidden" }}>
+      <Animated.View
+        style={[
+          {
+            gap: 5,
+            paddingVertical: 8,
+            paddingHorizontal: 16,
+          },
+          animStyle,
+        ]}
+      >
+        {children}
+      </Animated.View>
+    </Card>
+  );
+}
+
 export const PixelInfoCardModeContext = React.createContext<
   "normal" | "expanded"
 >("normal");
@@ -175,23 +241,21 @@ export function PixelInfoCard({ children, pixelInfo }: PixelInfoCardProps) {
   const { t } = useTranslation();
   const props = { pixel: pixelInfo, t };
   return (
-    <Card>
-      <Card.Content style={{ gap: 5 }}>
-        <PixelName pixel={pixelInfo} />
-        <PixelInfoCardModeContext.Consumer>
-          {(mode) => (
-            <View style={{ height: mode === "expanded" ? undefined : 0 }}>
-              <PixelMoreInfo {...props} />
-            </View>
-          )}
-        </PixelInfoCardModeContext.Consumer>
-        <BaseHStack w="100%" justifyContent="space-around">
-          <PixelRssi {...props} />
-          <PixelBattery {...props} />
-          <PixelRollState {...props} />
-        </BaseHStack>
-        {children}
-      </Card.Content>
-    </Card>
+    <BlinkingCard pixel={pixelInfo}>
+      <PixelName pixel={pixelInfo} />
+      <PixelInfoCardModeContext.Consumer>
+        {(mode) => (
+          <View style={{ height: mode === "expanded" ? undefined : 0 }}>
+            <PixelMoreInfo {...props} />
+          </View>
+        )}
+      </PixelInfoCardModeContext.Consumer>
+      <BaseHStack w="100%" justifyContent="space-around">
+        <PixelRssi {...props} />
+        <PixelBattery {...props} />
+        <PixelRollState {...props} />
+      </BaseHStack>
+      {children}
+    </BlinkingCard>
   );
 }
