982a23fa1b438fafabcc619a6f98136d82c56968
Olivier Basille <obasille@hotmail.com>
2024-02-27 21:41:09 +0100

    fix(pixels-app): don't show gradient UI for animations that only have "intensity" gradients

--- apps/pixels-app/src/features/store/library/animationGradient.ts
+++ apps/pixels-app/src/features/store/library/animationGradient.ts
@@ -8,6 +8,17 @@ const angleGradient: readonly string[] = [
   "c89728f7-4ddf-4d05-818d-e57e200f9741",
   "d0e73d74-333e-4634-b30e-e078b376d789",
 ];
+const noGradient: readonly string[] = [
+  "ab516f76-fe89-48a6-8b11-35f997d31197",
+  "ebfc1dd5-ec82-45ac-a653-db2fbb96e5c1",
+  "e655e6e1-32c3-407b-a154-15ceef54c9f3",
+  "b28682d0-fffd-4a07-b723-dbb127309c23",
+  "17c75b18-b186-4a4e-ade0-bf94f658d6db",
+  "61785d78-a642-461f-8dc1-1f0fb2dddd29",
+  "5575de5a-2059-48c1-a08a-99ecc19b775c",
+  "c30e296b-422a-4713-9bb4-85c2fcee56d8",
+  "42613ce9-12fb-4973-8e38-cc0765d980f1",
+];
 
 export function getAnimationGradient(
   anim?: Profiles.Animation
@@ -21,7 +32,7 @@ export function getAnimationGradient(
       return anim.gradient;
     } else if (angleGradient.includes(anim.uuid)) {
       return anim.angleGradient;
-    } else {
+    } else if (!noGradient.includes(anim.uuid)) {
       return anim.axisGradient;
     }
   } else if (anim instanceof Profiles.AnimationCycle) {
