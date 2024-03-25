bfe5cc429c961be532177e662398704b3b673fc6
Olivier Basille <obasille@hotmail.com>
2024-03-25 20:57:29 +0100

    fix(pixels-app): blink die green on selection, rather than magenta which is also used to indicate an error

--- apps/pixels-app/src/features/dice/blinkDie.ts
+++ apps/pixels-app/src/features/dice/blinkDie.ts
@@ -7,5 +7,5 @@ import {
 
 export function blinkDie(die?: Pick<PixelInfo, "pixelId">): void {
   const pixel = !die || die instanceof Pixel ? die : getPixel(die.pixelId);
-  pixel?.blink(Color.dimMagenta, { duration: 1000, count: 2 }).catch(() => {});
+  pixel?.blink(Color.dimGreen, { duration: 1000, count: 2 }).catch(() => {});
 }
