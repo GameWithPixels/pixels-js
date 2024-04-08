9555d4a935e8f051dd7439a7d9aea158d483f30a
Olivier Basille <obasille@hotmail.com>
2024-04-08 09:36:22 +0200

    docs(core-anim): documents AnimationFlags

--- packages/pixels-core-animation/src/animations/AnimationFlags.ts
+++ packages/pixels-core-animation/src/animations/AnimationFlags.ts
@@ -1,12 +1,16 @@
 import { enumFlag } from "@systemic-games/pixels-core-utils";
 
 /**
+ * Flags for the animations, they can be combined.
  * @category Animation
  * @enum
  */
 export const AnimationFlagsValues = {
+  /** Default behavior. */
   none: 0,
+  /** Make the animation travel around the dice, only available for the Rainbow animation. */
   traveling: enumFlag(0),
+  /** Play animation is using LED indices, not face indices. */
   useLedIndices: enumFlag(),
 } as const;
 
