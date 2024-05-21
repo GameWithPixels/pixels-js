18bbf04384deba83ba49ec9d8d8b107991441b52
Olivier Basille <obasille@hotmail.com>
2024-05-21 15:44:54 +0200

    chore(toolbox): remove old unused dice management files

--- apps/pixels-toolbox/src/features/dice/ListChangeEvent.ts
+++
@@ -1,8 +0,0 @@
-export type ListChangeOperation = "add" | "remove";
-
-export interface ListChange<T> {
-  operation: ListChangeOperation;
-  item: T;
-}
-
-export class ListChangeEvent<T> extends CustomEvent<ListChange<T>> {}
