b24cdfc9ea064ec6ea930e0bf9a2faedd3df4e96
Olivier Basille <obasille@hotmail.com>
2024-06-22 23:14:34 +0200

    chore(pixels-app): v2.3.0

--- apps/pixels-app/app.config.ts
+++ apps/pixels-app/app.config.ts
@@ -3,8 +3,8 @@ const prod =
   !process.env.SYSTEMIC_PX_DEV &&
   process.env.EAS_BUILD_PROFILE !== "development";
 
-const version = "2.2.2"; // Version number must have 3 parts
-const buildNumber = 2202;
+const version = "2.3.0"; // Version number must have 3 parts
+const buildNumber = 2300;
 
 const config = {
   expo: {
