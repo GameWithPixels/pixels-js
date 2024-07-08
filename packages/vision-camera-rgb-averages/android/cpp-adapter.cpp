3ca51a7ae16e98eaa3442f939bacebb4f0a2179c
Olivier Basille <obasille@hotmail.com>
2024-07-08 23:36:49 +0200

    feat(rgb-avg): add vision camera C++ frame processor

--- packages/vision-camera-rgb-averages/android/cpp-adapter.cpp
+++ packages/vision-camera-rgb-averages/android/cpp-adapter.cpp
@@ -1,8 +1,12 @@
 #include <jni.h>
+#include <jsi/jsi.h>
 #include "systemic-games-vision-camera-rgb-averages.h"
 
 extern "C"
-JNIEXPORT jdouble JNICALL
-Java_com_systemicgames_visioncamerargbaverages_VisionCameraRgbAveragesModule_nativeMultiply(JNIEnv *env, jclass type, jdouble a, jdouble b) {
-    return systemicgames_visioncamerargbaverages::multiply(a, b);
+JNIEXPORT void JNICALL
+Java_com_systemicgames_visioncamerargbaverages_VisionCameraRgbAveragesModule_nativeInstall(JNIEnv *env, jclass type, jlong jsi) {
+    auto runtime = reinterpret_cast<facebook::jsi::Runtime *>(jsi);
+    if (runtime) {
+        systemicgames_visioncamerargbaverages::install(*runtime);
+    }
 }
--- packages/vision-camera-rgb-averages/ios/VisionCameraRgbAverages.mm
+++ packages/vision-camera-rgb-averages/ios/VisionCameraRgbAverages.mm
@@ -1,18 +1,32 @@
 #import "VisionCameraRgbAverages.h"
+#import <React/RCTBridge+Private.h>
+#import <React/RCTUtils.h>
+#import <jsi/jsi.h>
+#import "systemic-games-vision-camera-rgb-averages.h"
 
 @implementation VisionCameraRgbAverages
+
+@synthesize bridge = _bridge;
+@synthesize methodQueue = _methodQueue;
+
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_EXPORT_METHOD(multiply:(double)a
-                  b:(double)b
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-    NSNumber *result = @(systemicgames_visioncamerargbaverages::multiply(a, b));
++ (BOOL)requiresMainQueueSetup {
+    return YES;
+}
 
-    resolve(result);
+- (void)setBridge:(RCTBridge *)bridge {
+    _bridge = bridge;
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install) {
+    RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
+    if (!cxxBridge.runtime) {
+        return @false;
+    } else {
+        systemicgames_visioncamerargbaverages::install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
+        return @true;
+    }
 }
 
 // Don't compile this code when we build for the old architecture.
