bebd389d41637fe1ad43057b0f78352a805651a7
Olivier Basille <obasille@hotmail.com>
2024-07-11 07:08:39 +0200

    feat(toolbox): re-enable Pixel id decoder

--- apps/pixels-toolbox/src/hooks/usePixelIdDecoderFrameProcessor.ts
+++ apps/pixels-toolbox/src/hooks/usePixelIdDecoderFrameProcessor.ts
@@ -1,15 +1,21 @@
-import { getImageRgbAverages } from "@systemic-games/vision-camera-rgb-averages";
+import {
+  getRgbAveragesFrameProcessor,
+  ImageRgbAverages,
+  installFrameProcessor,
+} from "@systemic-games/vision-camera-rgb-averages";
 import React from "react";
-import { runOnJS } from "react-native-reanimated";
 import {
   Frame,
   ReadonlyFrameProcessor,
   useFrameProcessor,
 } from "react-native-vision-camera";
+import { Worklets } from "react-native-worklets-core";
 
 import { PixelIdDecoderState, usePixelIdDecoder } from "./usePixelIdDecoder";
 
 export type FrameProcessor = (frame: Frame) => void;
+
+installFrameProcessor();
 
 export function usePixelIdDecoderFrameProcessor(): [
   ReadonlyFrameProcessor,
@@ -21,20 +27,36 @@ export function usePixelIdDecoderFrameProcessor(): [
   // PixelId decoder
   const [decoderState, decoderDispatch] = usePixelIdDecoder();
 
+  // Process the RGB averages on the JS thread
+  const processOnJS = Worklets.createRunOnJS(
+    (rgbAverages: ImageRgbAverages) => {
+      try {
+        decoderDispatch({ rgbAverages });
+      } catch (error) {
+        setLastError(error instanceof Error ? error : new Error(String(error)));
+      }
+    }
+  );
+
+  // Get the frame processor
+  const frameProc = getRgbAveragesFrameProcessor();
+
   // Get the average R, G and B for each image captured by the camera
   const frameProcessor = useFrameProcessor(
     (frame) => {
       "worklet";
-      const result = getImageRgbAverages(frame, {
-        maxPixelsToProcess: 320 * 240, // Limit number of processed pixels for performance reason
-      });
-      if (typeof result === "string") {
-        setLastError(new Error(result));
-      } else {
-        runOnJS(decoderDispatch)({ rgbAverages: result });
+      if (frameProc) {
+        // console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+        const rgbAverages = frameProc(frame, {
+          subSamplingX: 2,
+          subSamplingY: 2,
+        });
+        if (rgbAverages) {
+          processOnJS(rgbAverages);
+        }
       }
     },
-    [decoderDispatch]
+    [frameProc, processOnJS]
   );
 
   return [frameProcessor, decoderState, lastError];
