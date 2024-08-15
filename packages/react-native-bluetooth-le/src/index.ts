ddae8c69349d2c08cb179f72c6dbf1f9977061f6
Olivier Basille <obasille@hotmail.com>
2024-08-15 12:07:19 +0200

    feat(rn-ble): remove BleScanner class which wasn't doing much

--- packages/react-native-bluetooth-le/src/index.ts
+++ packages/react-native-bluetooth-le/src/index.ts
@@ -2,6 +2,5 @@ export * from "./events";
 export * from "./errors";
 export * from "./BluetoothLE";
 export * from "./Central";
-export * from "./BleScanner";
 export * from "./requestPermissions";
 export * from "./getNativeErrorCode";
