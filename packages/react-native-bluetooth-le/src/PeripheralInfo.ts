bcadb10b6f1ac44f985572a56c32dfba4bfcd137
Olivier Basille <obasille@hotmail.com>
2024-08-16 13:14:59 +0200

    refactor(rn-ble): factorize Central event raising code

--- packages/react-native-bluetooth-le/src/PeripheralInfo.ts
+++ packages/react-native-bluetooth-le/src/PeripheralInfo.ts
@@ -20,6 +20,3 @@ export interface PeripheralInfo {
     (ev: PeripheralCharacteristicValueChangedEvent) => void
   >;
 }
-
-// Keep list of peripherals in a separate file so it is not reset by Fast Refresh after a change in Central
-export const CentralPeripheralsMap: Map<string, PeripheralInfo> = new Map();
