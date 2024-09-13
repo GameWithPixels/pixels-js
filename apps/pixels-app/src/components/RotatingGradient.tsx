1dab601f72a2f42d87ff7a1920be388b24bd5e98
Olivier Basille <obasille@hotmail.com>
2024-09-13 13:43:11 +0200

    refactor(pixels-app): optimize rotating gradient to use less CPU time

--- apps/pixels-app/src/components/RotatingGradient.tsx
+++ apps/pixels-app/src/components/RotatingGradient.tsx
@@ -3,83 +3,89 @@ import {
   Canvas,
   Color,
   LinearGradient,
-  RoundedRect,
+  Rect,
   vec,
 } from "@shopify/react-native-skia";
 import React from "react";
-import { StyleSheet, View, ViewProps } from "react-native";
-import {
+import { View, ViewProps } from "react-native";
+import Animated, {
   Easing,
   ReduceMotion,
-  useDerivedValue,
+  useAnimatedStyle,
   useSharedValue,
   withRepeat,
   withTiming,
 } from "react-native-reanimated";
 
+import { AppStyles } from "~/app/styles";
+
 export function RotatingGradient({
   children,
   style,
   colors,
+  onLayout,
   ...props
 }: ViewProps & { colors: AnimatedProp<Color[]> }) {
-  // Size of the view
-  const size = useSharedValue({ width: 0, height: 0 });
-  const width = useDerivedValue(() => size.value.width);
-  const height = useDerivedValue(() => size.value.height);
-
   // Rotate the gradient
   const angle = useSharedValue(0);
   React.useEffect(() => {
     angle.value = withRepeat(
       withTiming(2 * Math.PI, {
-        duration: 2000,
+        duration: 3000,
         easing: Easing.linear,
         reduceMotion: ReduceMotion.Never,
       }),
       -1 // true
     );
   }, [angle]);
+  const rotation = useAnimatedStyle(() => ({
+    transformOrigin: ["50%", "50%", 0],
+    transform: [{ rotate: `${angle.value}rad` }],
+  }));
 
-  // Calculate the start and end points of the gradient
-  const start = useDerivedValue(() =>
-    vec(
-      0.5 * width.value * (1 + Math.cos(angle.value)),
-      0.5 * height.value * (1 + Math.sin(angle.value))
-    )
-  );
-  const end = useDerivedValue(() =>
-    vec(
-      0.5 * width.value * (1 + Math.cos(angle.value + Math.PI)),
-      0.5 * height.value * (1 + Math.sin(angle.value + Math.PI))
-    )
-  );
-
-  // Extract the borderRadius from the style
-  const flatStyle = React.useMemo(() => StyleSheet.flatten(style), [style]);
-  const radius = useSharedValue(
-    typeof flatStyle.borderRadius === "number" ? flatStyle.borderRadius : 0
-  );
-  React.useEffect(() => {
-    if (typeof flatStyle.borderRadius === "object") {
-      const animatedRadius = flatStyle.borderRadius;
-      const id = animatedRadius.addListener(
-        ({ value }: { value: number }) => (radius.value = value)
-      );
-      return () => animatedRadius.removeListener(id);
-    }
-  }, [flatStyle.borderRadius, radius]);
+  // Size of the view
+  const [size, setSize] = React.useState({ width: 0, height: 0 });
+  const { r, dx, dy } = React.useMemo(() => {
+    const r = Math.sqrt(2 * Math.max(size.width, size.height) ** 2);
+    const dx = (r - size.width) / 2;
+    const dy = (r - size.height) / 2;
+    return { r, dx, dy };
+  }, [size]);
 
   return (
-    <View style={style} {...props}>
-      <Canvas
-        style={{ position: "absolute", width: "100%", height: "100%" }}
-        onSize={size}
+    <View
+      style={style}
+      onLayout={(ev) => {
+        onLayout?.(ev);
+        setSize({
+          width: ev.nativeEvent.layout.width,
+          height: ev.nativeEvent.layout.height,
+        });
+      }}
+      {...props}
+    >
+      <Animated.View
+        style={[
+          {
+            position: "absolute",
+            left: -dx,
+            right: -dx,
+            top: -dy,
+            bottom: -dy,
+          },
+          rotation,
+        ]}
       >
-        <RoundedRect x={0} y={0} width={width} height={height} r={radius}>
-          <LinearGradient start={start} end={end} colors={colors} />
-        </RoundedRect>
-      </Canvas>
+        <Canvas style={AppStyles.fullSize}>
+          <Rect x={0} y={0} width={r} height={r}>
+            <LinearGradient
+              start={vec(dx, dy)}
+              end={vec(dx + size.width, dy + size.height)}
+              colors={colors}
+            />
+          </Rect>
+        </Canvas>
+      </Animated.View>
       {children}
     </View>
   );
