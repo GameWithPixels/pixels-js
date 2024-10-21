e732a6986fd84d487600832616f88bff04b6e4b9
Olivier Basille <obasille@hotmail.com>
2024-10-21 17:02:05 +0200

    feat(pixels-app): add more properties to the JSON send by web requests

--- apps/pixels-app/src/features/profiles/getWebRequestURL.ts
+++ apps/pixels-app/src/features/profiles/getWebRequestURL.ts
@@ -1,22 +1,52 @@
 import { PixelInfo } from "@systemic-games/react-native-pixels-connect";
+import * as Application from "expo-application";
 
 export type ActionMakeWebRequestPayload = Readonly<{
-  pixelName: string;
+  timestamp: string;
+  appVersion: string;
+  appBuild: string;
   profileName: string;
   actionValue: string;
+  pixelName: string;
   faceValue: number;
-}>;
+  faceIndex: number;
+  firmwareTimestamp: number;
+}> &
+  Pick<
+    PixelInfo,
+    | "pixelId"
+    | "ledCount"
+    | "colorway"
+    | "dieType"
+    | "rssi"
+    | "batteryLevel"
+    | "isCharging"
+    | "rollState"
+  >;
 
 export function getWebRequestPayload(
-  pixel: Pick<PixelInfo, "name" | "currentFace">,
+  pixel: Omit<PixelInfo, "systemId">,
   profileName: string,
   actionValue: string
 ): ActionMakeWebRequestPayload {
   return {
-    pixelName: pixel.name,
+    timestamp: new Date().toISOString(),
+    appVersion: Application.nativeApplicationVersion ?? "0",
+    appBuild: Application.nativeBuildVersion ?? "0",
     profileName,
     actionValue,
+    pixelName: pixel.name,
+    pixelId: pixel.pixelId,
     faceValue: pixel.currentFace,
+    faceIndex: pixel.currentFaceIndex,
+    ledCount: pixel.ledCount,
+    colorway: pixel.colorway,
+    dieType: pixel.dieType,
+    firmwareTimestamp: pixel.firmwareDate.getTime(),
+    rssi: pixel.rssi,
+    batteryLevel: pixel.batteryLevel,
+    isCharging: pixel.isCharging,
+    rollState: pixel.rollState,
   };
 }
 
