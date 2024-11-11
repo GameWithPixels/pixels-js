1fa1f254561a9403e1c6d88f1f3dead4d99217e6
Olivier Basille <obasille@hotmail.com>
2024-11-11 11:36:14 +0100

    fix(pixels-app): make FileReader load base 64 files of types other than just data:application

--- apps/pixels-app/src/features/render3d/readAsArrayBuffer.ts
+++ apps/pixels-app/src/features/render3d/readAsArrayBuffer.ts
@@ -12,9 +12,13 @@ FileReader.prototype.readAsArrayBuffer = function (blob: Blob) {
   const fr = new FileReader();
   fr.onloadend = () => {
     if (typeof fr.result === "string") {
-      const data = fr.result.substring(
-        "data:application/octet-stream;base64,".length
-      );
+      const index = fr.result.indexOf(";base64,");
+      if (index === -1) {
+        throw new Error(
+          `Data URL did not contain base64 part, starts with: ${fr.result.substring(0, 30)}`
+        );
+      }
+      const data = fr.result.substring(index + 8);
       // @ts-ignore Property '_result' does not exist on type 'FileReader'
       this._result = toByteArray(data).buffer; // Faster than Buffer.from()
       // @ts-ignore Property '_setReadyState' does not exist on type 'FileReader'
