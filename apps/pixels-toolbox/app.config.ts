9a35ddbe531194df0bb524890c6598c4c6d8c177
Olivier Basille <obasille@hotmail.com>
2024-11-19 17:26:14 +0100

    chore(toolbox): release 5.36 still with FW 2024-01-02

--- apps/pixels-toolbox/app.config.ts
+++ apps/pixels-toolbox/app.config.ts
@@ -9,7 +9,7 @@ const config = {
     slug: prod ? "pixels-toolbox" : "pixels-toolbox-dev",
     owner: "gamewithpixels",
     runtimeVersion: "48.9", // Major is Expo version, minor is native code revision
-    version: "5.35.0", // Version number must have 3 parts
+    version: "5.36.0", // Version number must have 3 parts
     platforms: ["ios", "android"],
     orientation: "portrait",
     icon: prod ? "./assets/images/icon.png" : "./assets/images/icon-dev.png",
