3fd870d5bc2b2c126d2c9896bb31fdb4781e945f
Olivier Basille <obasille@hotmail.com>
2024-12-07 13:06:06 +0100

    fix(pixels-react): fix useMPCProp() not updating when mpc instance change

--- packages/pixels-react/src/useMPCProp.ts
+++ packages/pixels-react/src/useMPCProp.ts
@@ -5,25 +5,26 @@ import { useForceUpdate } from "./useForceUpdate";
 
 /**
  * React Hook that updates when the given MPC's prop changes.
- * @param pixel The MPC for which to watch the prop.
+ * @param mpc The MPC for which to watch the prop.
  * @param prop The prop to watch.
  * @returns The current value of the given prop on the given MPC.
  * @remarks Use {@link usePixelEvent} to watch the RSSI value.
  */
 export function useMPCProp<T extends keyof MPCMutableProps>(
-  pixel: MPC | undefined,
+  mpc: MPC | undefined,
   prop: T
 ): MPCMutableProps[T] | undefined {
   const forceUpdate = useForceUpdate();
 
   // Subscribe to prop event to trigger a React update when the value changes
   React.useEffect(() => {
-    pixel?.addPropertyListener(prop, forceUpdate);
+    forceUpdate();
+    mpc?.addPropertyListener(prop, forceUpdate);
     return () => {
-      pixel?.removePropertyListener(prop, forceUpdate);
+      mpc?.removePropertyListener(prop, forceUpdate);
     };
-  }, [forceUpdate, pixel, prop]);
+  }, [forceUpdate, mpc, prop]);
 
   // Return the latest value of the prop
-  return pixel?.[prop];
+  return mpc?.[prop];
 }
